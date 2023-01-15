7192bd21b98f3519d15f571f7b5822fe7e6d215f
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-15 23:27:23 +0700

    Create new div with border and appendCHild P and H1 to it

--- js-file.js
+++ js-file.js
@@ -13,7 +13,20 @@ createH3.style.color='blue';
 container.appendChild(createH3);
 
 // a <div> with a black border and pink background color with the following elements inside of it:
+const createDiv = document.createElement('div');
+createDiv.style.border = "thin solid black";
+createDiv.style.backgroundColor='pink';
+
 // another <h1> that says “I’m in a div”
+const createH1 = document.createElement('h1');
+createH1.textContent="I'm in a div";
+createDiv.appendChild(createH1);
+
 // a <p> that says “ME TOO!”
+const anotherP = document.createElement('p');
+anotherP.textContent="ME TOO!";
+createDiv.appendChild(anotherP);
+
 // Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
 
+container.appendChild(createDiv);
